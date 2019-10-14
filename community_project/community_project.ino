07161c002bf5f8e2f4185a3d028915b59bf73c8e
Tokisaki Kurumi <minhkhoile2001nsg@gmail.com>
2019-10-14 16:16:26 +0700

    Final version of the source code to control basket

--- community_project/community_project.ino
+++ community_project/community_project.ino
@@ -2,15 +2,15 @@
 #include "DFRobotDFPlayerMini.h"
 
 #define BUTTON 13
+int prevBtnState = 0;
+int btnState = 0;
 
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
-int flag = 0;
-void printDetail(uint8_t type, int value);
+int flag = 1;
 
 void setup()
 {
-  flag = 1;
   pinMode(BUTTON, OUTPUT);
   mySoftwareSerial.begin(9600);
   Serial.begin(115200);
@@ -29,91 +29,24 @@ void setup()
   }
   Serial.println(F("DFPlayer Mini online."));
 
-  myDFPlayer.volume(20);  //Set volume value. From 0 to 30
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
 }
 
 void loop()
 {
-  int prevBtnState = 0;
-  int btnState = digitalRead(BUTTON);
-  if (btnState != prevBtnState)
-  {
-    if (btnState == HIGH && flag < 0) {
-      // button is pressed
-      myDFPlayer.play(2); // Thank you
-      flag *= -1;
-    }
-    if (btnState == LOW) {
-      // open the basket
-      myDFPlayer.play(1); // Please give me trash
-      flag *= -1;
-    }
+  btnState = digitalRead(BUTTON);
+  if (btnState == 1 && btnState != prevBtnState) {
+    // open the basket
+    Serial.println("Is pressed");
+    myDFPlayer.play(3); // Please give me trash
+    delay(2000);
+    flag *= -1;
   }
-
+  if (btnState == 0 && flag < 0) {
+    Serial.println("Is not pressed");
+    myDFPlayer.play(2);
+    delay(1500);
+    flag *= -1;
+  }
   prevBtnState = btnState;
-  if (myDFPlayer.available()) {
-    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
-  }
-}
-
-void printDetail(uint8_t type, int value) {
-  switch (type) {
-    case TimeOut:
-      Serial.println(F("Time Out!"));
-      break;
-    case WrongStack:
-      Serial.println(F("Stack Wrong!"));
-      break;
-    case DFPlayerCardInserted:
-      Serial.println(F("Card Inserted!"));
-      break;
-    case DFPlayerCardRemoved:
-      Serial.println(F("Card Removed!"));
-      break;
-    case DFPlayerCardOnline:
-      Serial.println(F("Card Online!"));
-      break;
-    case DFPlayerUSBInserted:
-      Serial.println("USB Inserted!");
-      break;
-    case DFPlayerUSBRemoved:
-      Serial.println("USB Removed!");
-      break;
-    case DFPlayerPlayFinished:
-      Serial.print(F("Number:"));
-      Serial.print(value);
-      Serial.println(F(" Play Finished!"));
-      break;
-    case DFPlayerError:
-      Serial.print(F("DFPlayerError:"));
-      switch (value) {
-        case Busy:
-          Serial.println(F("Card not found"));
-          break;
-        case Sleeping:
-          Serial.println(F("Sleeping"));
-          break;
-        case SerialWrongStack:
-          Serial.println(F("Get Wrong Stack"));
-          break;
-        case CheckSumNotMatch:
-          Serial.println(F("Check Sum Not Match"));
-          break;
-        case FileIndexOut:
-          Serial.println(F("File Index Out of Bound"));
-          break;
-        case FileMismatch:
-          Serial.println(F("Cannot Find File"));
-          break;
-        case Advertise:
-          Serial.println(F("In Advertise"));
-          break;
-        default:
-          break;
-      }
-      break;
-    default:
-      break;
-  }
-
 }
